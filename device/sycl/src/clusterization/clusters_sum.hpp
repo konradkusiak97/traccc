78a21057a530f2e4a764ec23f5b4f033d220a8ee
Konrad Kusiak <k.a.kusiak@se20.qmul.ac.uk>
2022-05-05 10:31:28 +0200

    Experimenting with the exact sizes for each cluster

--- device/sycl/src/clusterization/clusters_sum.hpp
+++ device/sycl/src/clusterization/clusters_sum.hpp
@@ -12,20 +12,18 @@
 #include "traccc/edm/cell.hpp"
 
 // Vecmem include(s).
-#include <vecmem/containers/data/jagged_vector_view.hpp>
 #include <vecmem/memory/memory_resource.hpp>
-#include <vecmem/memory/unique_ptr.hpp>
+#include <vecmem/containers/data/jagged_vector_view.hpp>
+#include <vecmem/containers/data/vector_view.hpp>
 
 namespace traccc::sycl {
 
-/// Forward declaration of clusters sum function
+/// Forward declaration of component connection function
 ///
 void clusters_sum(
-    const cell_container_types::host& cells_per_event,
+    const host_cell_container& cells_per_event,
     vecmem::data::jagged_vector_view<unsigned int> sparse_ccl_indices_view,
-    unsigned int& total_clusters,
     vecmem::data::vector_view<std::size_t> cluster_prefix_sum_view,
-    vecmem::data::vector_view<std::size_t> clusters_per_module_view,
     vecmem::memory_resource& resource, queue_wrapper queue);
 
 }  // namespace traccc::sycl
